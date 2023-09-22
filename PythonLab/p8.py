df73aa8e17ec6ea166cc982dd42ade3342986ec4
Vinayak-Thakur <vinayak.cmrit@gmail.com>
2023-09-22 12:48:49 +0530

    Update p8.py

--- PythonLab/p8.py
+++ PythonLab/p8.py
@@ -1,29 +1,41 @@
-class Palindrome :
-    def __init__(self, data) :
-        self.data = str(data)
-    def ispalindrome(self, data ) :
-        y = len(self.data)-1
-        palin = True
-        for x in range(int(y/2)) :
-            if self.data[x]!=self.data[y] :
-                palin = False
-                break
-            y -= 1
-        if palin:
-            return True
-        else :
-            return False
-        
-n = int(input("enter the number "))
-n1 = Palindrome(n)
-if n1.ispalindrome(n) :
-    print(f"yes {n} is a palindrome")
-else :
-    print("not a palindrome ")
+class PalindomeChecker:
 
-n2= input ("enter the string ")
-s1= Palindrome(n2)
-if s1.ispalindrome(n2) :
-    print("yes palindrome ")
-else :
-    print("not a palindrome")
+  def __init__(self, value):
+    self.value = value
+
+  def is_palindrome():
+    pass
+
+
+class IntegerChecker(PalindomeChecker):
+
+  def is_palindrome(self):
+    copy = self.value
+
+    rev = 0
+    while copy > 0:
+      rev = (rev * 10) + (copy % 10)
+      copy //= 10
+
+    return rev == self.value
+  
+class StringChecker(PalindomeChecker):
+
+  def is_palindrome(self):
+    
+    cleaned_string = "".join(self.value.split()).lower()
+
+    return cleaned_string == cleaned_string[::-1]
+
+
+value = input("Enter value: ")
+
+if value.isdigit():
+  v = IntegerChecker(int(value))
+else:
+  v = StringChecker(value)
+
+if v.is_palindrome():
+  print("It is palindrome")
+else:
+  print("It is not a palindrome")
