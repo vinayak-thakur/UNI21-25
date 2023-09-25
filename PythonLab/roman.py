4abf019de6d154c8064502769d952a639fe6a88e
Vinayak-Thakur <vinayak.cmrit@gmail.com>
2023-09-25 19:24:29 +0530

    Update roman.py

--- PythonLab/roman.py
+++ PythonLab/roman.py
@@ -1,11 +1,24 @@
-roman = input("Enter roman number: ")
-roman_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-result = 0
+import re
+import string
 
-for i in range(len(roman)):
-    if i > 0 and roman_dict[roman[i]] > roman_dict[roman[i - 1]]:
-        result += roman_dict[roman[i]] - 2 * roman_dict[roman[i - 1]]
-    else:
-        result += roman_dict[roman[i]]
+def valid_roman(roman):
+    return bool(re.search(r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$",roman))
 
-print(f"Integer value is: {result}")
+
+def roman_to_integer(s):
+        roman_no = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        integer_no = 0
+        for i in range(len(s)):
+            if i > 0 and roman_no[s[i]] > roman_no[s[i - 1]]:
+                integer_no += roman_no[s[i]] - 2 * roman_no[s[i - 1]]
+            else:
+                integer_no += roman_no[s[i]]
+        return integer_no
+
+
+roman=input("enter the roman numeral(IVXLCDM)")
+if valid_roman(roman):
+     integer_no = roman_to_integer(roman)
+     print('the equivalent',integer_no)
+else:
+     print("Inavalid roman numeral")
